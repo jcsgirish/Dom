89ab03a6c2d4a7c969af072f06b6ac048168ca49
jcsgirish <jcsgirish7@gmail.com>
2023-01-21 23:39:22 +0530

    dom add items task

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+
+var headerTitle=document.getElementById("header-title")
+headerTitle.style.borderBottom = "solid 5px black";
+
+
+var items=document.getElementsByClassName("title")
+items[0].style.color = "green"
+items[0].style.fontWeight="bold"
