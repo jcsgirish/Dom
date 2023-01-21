9e3ae77375f487d47042a4443cf76fc3dd75e6f6
jcsgirish <jcsgirish7@gmail.com>
2023-01-22 01:17:03 +0530

    task3 dom

---
+++ dom3.js
@@ -0,0 +1,17 @@
+
+
+
+var lii=document.getElementsByTagName("li")
+
+for(var i=0;i<lii.length;i++)
+
+{
+    lii[i].style.backgroundColor="grey"
+}
+
+//var lii=document.getElementsByClassName("list-group-item")
+//for(var i=0;i<lii.length;i++)
+
+{
+  //lii[i].style.backgroundColor="grey"
+}
